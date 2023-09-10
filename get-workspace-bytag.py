a39a3af5073076858ead3e7649ee6de4257223d7
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-11 07:24:15 +1000

    add workspace by tag

---
+++ get-workspace-bytag.py
@@ -0,0 +1,40 @@
+# get workspace outputs
+import os
+import requests
+import json
+import logging
+
+def get_workspaces_by_tag(api_token, organization, querystring)::
+    workspaces_endpoint = f"https://app.terraform.io/api/v2/organizations/{organization}/workspaces"
+    headers = {
+        "Authorization": f"Bearer {api_token}",
+        "Content-Type": "application/vnd.api+json",
+    }
+
+    try:
+        response = requests.get(workspaces_endpoint, headers=headers params=querystring)
+        response.raise_for_status()
+        outputs = response.json()
+        return outputs
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to retrieve workspaces. Error: {e}")
+        return None
+
+# Set your Terraform Cloud API token
+api_token = os.getenv('TF_API_TOKEN')
+organization = os.getenv('TF_CLOUD_ORGANIZATION')
+querystring = {"search[tags]":"f5","sort":"name"}
+
+headers = {
+    "Authorization": f"Bearer {api_token}",
+    "Content-Type": "application/vnd.api+json",
+}
+
+# Retrieve workspace outputs as JSON
+outputs = get_workspaces_by_tag(api_token, organization, querystring)
+if outputs:
+    json_text = json.dumps(outputs)
+    print(json_text)
+else:
+    print("Failed to retrieve workspace outputs.")
